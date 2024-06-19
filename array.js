a49a7c9c78de40cfa592744a61c229460f028405
dag-bag <88625252+dag-bag@users.noreply.github.com>
2024-06-19 22:15:20 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,43 @@
+
+const arr = [1,2,3,4]
+// console.log(arr.filter((item)=> item < 3))
+// console.log(arr.find((item)=> item ===3))
+
+
+
+
+
+// const customFind = (arr,callback) =>{
+// let count = 0
+// let result;
+// 	for(let i = 0;i < arr.length;i++){
+//      count++
+//         if(callback(arr[i]))  {
+//         	result = arr[i]
+        	
+//         	break;
+//         }
+
+
+// 	}
+// 	console.log(0)
+// 	return result
+
+// }
+
+// let callback = (item) => (item > 2);
+// console.log(customFind(arr,callback))
+
+// const startEndDeleteMethood  = (arr,type)=> {
+
+// if(type === "start") {
+//  for(let i = 1;i < arr.length;i++) {
+//    arr[i -1] = arr[i]
+//  }
+// }
+
+// arr.length = arr.length - 1
+// return arr;
+// }
+
+// console.log(startEndDeleteMethood(arr,'end'))
